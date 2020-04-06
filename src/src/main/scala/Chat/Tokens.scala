c0cb0ddf86d10522dad4348d2b89b0250c86971a
Jael24 <jael.dubey@heig-vd.ch>
2020-04-06 17:47:57 +0200

    Implement step 1

--- src/src/main/scala/Chat/Tokens.scala
+++ src/src/main/scala/Chat/Tokens.scala
@@ -6,9 +6,19 @@ object Tokens {
   // Terms
   val BONJOUR: Token     = 0
   val JE: Token          = 1
+  val SOLDE: Token       = 26
+  val PRIX: Token        = 30
+  val USELESS: Token     = 31
+  // Asks
+  val COMBIEN: Token     = 27
+  val QUEL: Token        = 28
   // Actions
   val ETRE: Token        = 2
   val VOULOIR: Token     = 3
+  val APPELER: Token     = 23
+  val COMMANDER: Token   = 24
+  val CONNAITRE: Token   = 25
+  val COUTER: Token      = 29
   // Operators
   val ET: Token          = 4
   val OU: Token          = 5
@@ -21,6 +31,15 @@ object Tokens {
   val UNKNOWN: Token     = 11
   val EOL: Token         = 12
   // State of mind
-  val ASSOIFFE : Token   = 13
-  val AFFAME : Token     = 14
+  val ASSOIFFE: Token    = 13
+  val AFFAME: Token      = 14
+  // Brands
+  val MAISON: Token      = 15
+  val CAILLER: Token     = 16
+  val FARMER: Token      = 17
+  val BOXER: Token       = 18
+  val WITTEKOP: Token    = 19
+  val PUNKIPA: Token     = 20
+  val JACKHAMMER: Token  = 21
+  val TENEBREUSE: Token  = 22
 }
